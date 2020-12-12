26ba1058e6f88f232951df5f1c1b5fde4f6f414a
Jiasen Wu <jiasenwu@hotmail.com>
2020-12-12 21:07:21 +0100

    add askSession

--- src/MXNet/NN/Session.hs
+++ src/MXNet/NN/Session.hs
@@ -1,50 +1,64 @@
-{-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances      #-}
 {-# LANGUAGE FunctionalDependencies #-}
-{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE TemplateHaskell        #-}
 module MXNet.NN.Session where
 
-import RIO
-import qualified RIO.State as ST
-import qualified GHC.TypeLits as L
-import qualified Data.Type.Product as DT
-import qualified Data.Type.Index as DT
-import Data.Kind (Constraint)
+import           Data.Kind            (Constraint)
+import qualified Data.Type.Index      as DT
+import qualified Data.Type.Product    as DT
+import qualified GHC.TypeLits         as L
+import           RIO
+import qualified RIO.State            as ST
 
-import MXNet.Base
-import MXNet.NN.Types (Module, ModuleSet, ModuleState, TaggedModuleState)
-import MXNet.NN.TaggedState (liftSub, toPair)
+import           MXNet.Base
+import           MXNet.NN.TaggedState (liftSub, toPair)
+import           MXNet.NN.Types       (Module, ModuleSet, ModuleState,
+                                       TaggedModuleState)
 
 class Session (sess :: (* -> *) -> * -> *) (sst :: *) | sess -> sst, sst -> sess where
     type SessionDType sst
     type SessionHasModule sst (t :: L.Symbol) :: Constraint
-    withSession   :: MonadIO m => MVar sst -> sess m r -> m r
+    runSession    :: sess m r -> sst -> m (r, sst)
     subSession    :: MonadIO m => SessionHasModule sst t => Module t (SessionDType sst) m r -> sess m r
     sessionStates :: MonadIO m => sess m [(String, ModuleState (SessionDType sst))]
+
+class HasSessionRef e s | e -> s where
+    sessionRefL :: Lens' e (MVar s)
+
+askSession :: (MonadIO m, MonadReader e m, HasSessionRef e s, Session sess s)
+           => sess m r -> m r
+askSession proc = do
+    env <- ask
+    let var = env ^. sessionRefL
+    st <- liftIO $ takeMVar var
+    (rt, st) <- runSession proc st
+    liftIO $ putMVar var st
+    return rt
+
+withSession :: (MonadIO m, Session sess s)
+            => MVar s -> sess m r -> m r
+withSession var proc = do
+    st <- liftIO $ takeMVar var
+    (rt, st) <- runSession proc st
+    liftIO $ putMVar var st
+    return rt
 
 instance (DT.Every L.KnownSymbol t) => Session (ModuleSet t a) (DT.Prod (TaggedModuleState a) t) where
     type SessionDType (DT.Prod (TaggedModuleState a) t) = a
     type SessionHasModule (DT.Prod (TaggedModuleState a) t) t' = DT.Elem t t'
-    withSession var proc = do
-        st <- liftIO $ takeMVar var
-        (rt, st) <- ST.runStateT proc st
-        liftIO $ putMVar var st
-        return rt
+    runSession = ST.runStateT
     subSession = liftSub
     sessionStates = walk <$> ST.get
       where
         walk :: DT.Every L.KnownSymbol t => DT.Prod (TaggedModuleState a) t -> [(String, ModuleState a)]
-        walk DT.Ø = []
+        walk DT.Ø           = []
         walk (v DT.:< rest) = toPair v : walk rest
 
 instance (L.KnownSymbol t) => Session (Module t a) (TaggedModuleState a t) where
     type SessionDType (TaggedModuleState a t) = a
     type SessionHasModule (TaggedModuleState a t) t' = t ~ t'
-    withSession var proc = do
-        st <- liftIO $ takeMVar var
-        (rt, st) <- ST.runStateT proc st
-        liftIO $ putMVar var st
-        return rt
+    runSession = ST.runStateT
     subSession = id
     sessionStates = (:[]) . toPair <$> ST.get
 
